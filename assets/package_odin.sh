1a64db333f18ac504547fa715b5a1954be291c8f
marcs-feh <marcosfehlauer@protonmail.com>
2024-12-08 17:33:33 -0300

    fix

--- assets/package_odin.sh
+++ assets/package_odin.sh
@@ -7,7 +7,7 @@ sh build_static.sh
 sh remove_dlls.sh Odin
 strip Odin/odin
 zip -r -9 Odin.zip \
-	Odin/base Odin/core Odin/vendor \
+	Odin/odin Odin/base Odin/core Odin/vendor \
 	Odin/src Odin/.gitignore \
 	Odin/LICENSE Odin/README.md
 
